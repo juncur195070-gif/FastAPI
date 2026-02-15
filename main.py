30b6073d6f76e382f5e455c8fc3899ccb772b2ee
Junior <juncur.195070@gmail.com>
2026-02-15 10:04:17 -0500

    esto es de la primera clase

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from datetime import datetime
+import zoneinfo
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+    return {
+        "message": "Hello World"
+    }
+
+country_timezones = {
+    'CO': 'America/Bogota',
+    'MX': 'America/Mexico_City',
+    'AR': 'America/Argentina/Buenos_Aires',
+    'BR': 'America/Sao_Paulo',
+    'PE': 'America/Lima'
+}
+
+@app.get('/time/{iso_code}')
+def time(iso_code: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    return {
+        "time": datetime.now(tz)
+    }
